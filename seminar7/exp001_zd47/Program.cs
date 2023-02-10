110c201161e859d6e12e2f490cc2e19dfe5fc96a
Alexander <rooms90210@gmail.com>
2023-02-10 19:40:04 +0300

    задача 47

---
+++ seminar7/exp001_zd47/Program.cs
@@ -0,0 +1,51 @@
+﻿
+// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateArray(int rows, int columns)
+{
+    double[,] newFilledArray = new double[rows, columns];
+    Random Randomizer = new Random();
+
+    for (int y = 0; y < rows; y++)
+    {
+        for (int x = 0; x < columns; x++)
+        {
+            newFilledArray[y, x] = Randomizer.NextDouble() * Math.Pow(10, Randomizer.Next(2, 5));
+        }
+    }
+    return newFilledArray;
+}
+
+void PrintArray(double[,] arrayForPrint)
+{
+    for (int y = 0; y < arrayForPrint.GetLength(0); y++)
+    {
+        for (int x = 0; x < arrayForPrint.GetLength(1); x++)
+        {
+            System.Console.Write(" {0,15:F4} ", arrayForPrint[y, x]);
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int GetNumberInputFromUser()
+{
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Main()
+{
+    System.Console.Write("Введите количество строк: ");
+    int rowCount = GetNumberInputFromUser();
+    System.Console.Write("Введите количество столбцов: ");
+    int columnCount = GetNumberInputFromUser();
+    System.Console.WriteLine();
+    PrintArray(CreateArray(rowCount, columnCount));
+}
+
+Main();
